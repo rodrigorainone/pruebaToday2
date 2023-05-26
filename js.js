03e2f5da0327b1fb29454dbfc122311de04d6b9d
Tuky <rodri_cjs22@hotmail.com>
2023-05-26 04:42:20 -0300

    today 2

---
+++ js.js
@@ -0,0 +1,14 @@
+const holaItems = document.querySelectorAll('.hola');
+
+holaItems.forEach((holaItem) => {
+  const img = holaItem.querySelector('img');
+  const h1 = holaItem.querySelector('.displayh1');
+
+  img.addEventListener('mouseenter', () => {
+    h1.style.display = 'block';
+  });
+
+  img.addEventListener('mouseleave', () => {
+    h1.style.display = 'none';
+  });
+});
